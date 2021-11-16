44e7de1aa750f030a9fa2b7738765d026e8bd60d
DushveerSumaruth <dushveer11@gmail.com>
2021-11-16 16:28:51 +0400

    registrations controller completed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,33 @@
 class RegistrationsController < ApplicationController
+
+  def index
+   @registrations = Registration.all
+  end
+
+  def edit
+   @registration = Registration.find(params[:id])
+  end
+
+  def update
+    @registration = Registration.find(params[:id])
+    @registration.update(registration_params)
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @registration = Registration.new(registration_params)
+    if @registration.save
+      redirect_to registration_successfull_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def registration_params
+   params.require(:registration).permit(:car_make, :car_model, :chassis_number, :engine_number, :engine_capacity)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'registration_successfull', to: 'pages#registration_sucessfull'
+  resources :registrations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
