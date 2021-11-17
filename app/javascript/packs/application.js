a3dda4412643abd3286695bb0615e4ebcae85e4d
Beekun Muhammad Mozaffar <mozaf2905@gmail.com>
2021-11-17 22:54:49 +0400

    Added typedJS make changes to homepage adding legistlation make some front end add container on form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,12 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+
+import { loadDynamicBannerText } from '../components/banner';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  // [...]
+  loadDynamicBannerText();
+});
