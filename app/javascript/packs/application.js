624880cc2dbf02ac652f3d2b4b6e262504d0e6c6
Beekun Muhammad Mozaffar <mozaf2905@gmail.com>
2021-11-18 12:14:03 +0400

    added some front

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,10 +28,7 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
+
 
 
 import { loadDynamicBannerText } from '../components/banner';
