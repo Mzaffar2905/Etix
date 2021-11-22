edf80e8722b78ab2a223b0a42341b6fb2b3e37ac
Beekun Muhammad Mozaffar <mozaf2905@gmail.com>
2021-11-22 09:35:34 +0400

    adding analyst account

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(email:"mozaf2905@gmail.com", password:"qwerty", first_name:"Mozaffar", last_name:"Beekun",nic_number:"B290599381191E",license_number:"798413",phone_number:"+230 57453608",is_analyst: true)
