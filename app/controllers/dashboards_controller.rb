0379c599b8de41d294473246f89ac01fac6b6c77
Beekun Muhammad Mozaffar <mozaf2905@gmail.com>
2021-11-22 15:46:31 +0400

    Make changes in approval to validate and reject

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,5 @@
 class DashboardsController < ApplicationController
-  def show
+  def dashboard_analyst
     @user = current_user
     if @user.is_analyst?
     # @fines = Fine.where.not payment_done: true
@@ -8,16 +8,17 @@ class DashboardsController < ApplicationController
     # @car_registrations = CarRegistration.where.not payment_done: true
     # @general_questions = GeneralQuestion.all
 
-    @car_registrations = CarRegistration.where.not registration_approved: true
-    @declarations = Declaration.where.not declaration_approved: true
+    @car_registrations = CarRegistration.where registration_approved: "pending"
+    @declarations = Declaration.where declaration_approved: "pending"
     @fines = Fine.all
     @general_questions = GeneralQuestion.all
-    else render :index
+    else
+    render :dashboard_analyst
     end
   end
 
-  def index
+  def dashboard_user
     @user = current_user
-    @car_registrations = CarRegistration.where(user_id == current_user)
+    # @car_registrations = CarRegistration.where(user_id == current_user)
   end
 end
